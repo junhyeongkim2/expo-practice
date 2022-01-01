221e128faf49c30e2e33520b359a21b68801558a
junhyeongkim2 <ggprgrkjh@naver.com>
2022-01-02 01:41:37 +0900

    init

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>react-test changed1 </Text>
+      <Text>react-test changed </Text>
       <StatusBar style="auto" />
     </View>
   );
