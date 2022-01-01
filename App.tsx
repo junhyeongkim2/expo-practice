1987dac19190769e0da40f4ea924ed8189cbb0f8
junhyeongkim2 <ggprgrkjh@naver.com>
2022-01-02 02:01:12 +0900

    chore:masterchanged

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>react-test changed </Text>
+      <Text>react-test changed 123</Text>
       <StatusBar style="auto" />
     </View>
   );
